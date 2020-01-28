367a75fc0633d5c6c7c119f0665c1bd610ef77d8
GAGAN2608 <gaganaman.26@gmail.com>
2020-01-28 18:01:09 +0530

    edit method

--- src/App.js
+++ src/App.js
@@ -45,7 +45,18 @@ class App extends Component {
       });
   };
   handleEdit = id => {
-    console.log(`edit edit ${id}`);
+    //console.log(`edit edit ${id}`);
+    const filteredItems = this.state.items.filter(item => 
+      item.id !== id);
+
+    const selectedItem = this.state.items.find(item =>
+      item.id === id);
+      this.setState({
+        items : filteredItems,
+        item : selectedItem.title,
+        id : id,
+        editItem : true
+      })  
   };
   render() {
     return (
