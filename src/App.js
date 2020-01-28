89a2dde27a296e9fee77d540a2185dac2101516c
GAGAN2608 <gaganaman.26@gmail.com>
2020-01-28 17:09:20 +0530

    handle change and handle submit, todo item component structure

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-// showing vs-code github setup
 class App extends Component {
   state = {
-    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    items: [],
     id: uuid(),
     item: "",
     editItem: false
   };
   handleChange = e => {
-    console.log("handle change");
+    this.setState({
+      item: e.target.value
+    });
   };
   handleSubmit = e => {
-    console.log("handle Submit");
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
   };
   clearList = () => {
     console.log("clear list ");
   };
   handleDelete = id => {
-    console.log(`handle edit ${id}`);
+    console.log(`handle delete ${id}`);
   };
   handleEdit = id => {
     console.log(`edit edit ${id}`);
