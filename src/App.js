5fb5cf3ee4c5edf0fe56e12690764f77899d29b5
GAGAN2608 <gaganaman.26@gmail.com>
2020-01-28 16:31:54 +0530

    Input component structure

--- src/App.js
+++ src/App.js
@@ -1,61 +1,54 @@
-import React from 'react';
-import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
-
-function App() {
+import React, { Component } from "react";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+// showing vs-code github setup
+class App extends Component {
   state = {
-    items : [{
-      id : 1,
-      title : "wake up", 
-    },
-    {
-      id : 2,
-      title : "have breakfast"
-    }],
-    id : uuid(),
-    item :'',
-    editItem : false
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    id: uuid(),
+    item: "",
+    editItem: false
   };
-  handleChange = (e) => {
-    console.log('handle change');
+  handleChange = e => {
+    console.log("handle change");
   };
-  handleSubmit = (e) => {
-    console.log('handle submit');
+  handleSubmit = e => {
+    console.log("handle Submit");
   };
-  clearList = (e) => {
-    console.log('clear list');
+  clearList = () => {
+    console.log("clear list ");
   };
-  handleDelete = (id) => {
+  handleDelete = id => {
     console.log(`handle edit ${id}`);
   };
-  handleEdit = (id)=>{
+  handleEdit = id => {
     console.log(`edit edit ${id}`);
   };
-  return (
+  render() {
+    return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
-            <h3 className="text-captalize text-center">todo input 
-            </h3>      
-            <TodoInput 
-            item={this.state.item} 
-            handleChange={this.handleChange} 
-            handleSubmit={this.handleSubmit} 
-            editItem={this.state.editItem}
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
             />
             <TodoList
-            items={this.state.items}
-            clearList={this.clearList}
-            handleDelete={this.handleDelete}
-            handleEdit={this.handleEdit}
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
             />
           </div>
         </div>
       </div>
-  
-    );  
+    );
+  }
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class TodoItem extends Component {
-    render() {
-        return (
-            <div>
-                Hello from TodoItem.
-            </div>
-        )
-    }
+  render() {
+    return <div>hello from todo item</div>;
+  }
 }
